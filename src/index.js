342b17281980943660fd8c79bd77c2b2a5dc1c07
milorad996 <mips96.1@gmail.com>
2025-04-27 00:32:08 +0200

    new

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter basename="/auto-marketplace-front">
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter basename="/auto-marketplace-front">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
